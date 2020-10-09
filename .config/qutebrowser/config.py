84c5a3aba852e78b54e7ee0e8da6ce705c48cb9e
a2lqr4 <gustavoaraujob1@gmail.com>
2020-10-08 22:03:51 -0300

    Clean up default comments

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -6,41 +6,18 @@ config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}) AppleWebKit/53
 config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}; rv:71.0) Gecko/20100101 Firefox/71.0', 'https://docs.google.com/*')
 config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}; rv:71.0) Gecko/20100101 Firefox/71.0', 'https://drive.google.com/*')
 
-# Load images automatically in web pages.
-# Type: Bool
 config.set('content.images', True, 'chrome-devtools://*')
 config.set('content.images', True, 'devtools://*')
 
-# Enable JavaScript.
-# Type: Bool
 config.set('content.javascript.enabled', True, 'chrome-devtools://*')
 config.set('content.javascript.enabled', True, 'devtools://*')
 config.set('content.javascript.enabled', True, 'chrome://*/*')
 config.set('content.javascript.enabled', True, 'qute://*/*')
 
-# Open new windows in private browsing mode which does not record
-# visited pages.
-# Type: Bool
 c.content.private_browsing = True
 
-# Directory to save downloads to. If unset, a sensible OS-specific
-# default is used.
-# Type: Directory
 c.downloads.location.directory = '/home/HDD/Downloads'
 
-# When to show the statusbar.
-# Type: String
-# Valid values:
-#   - always: Always show the statusbar.
-#   - never: Always hide the statusbar.
-#   - in-mode: Show the statusbar when in modes other than normal mode.
 c.statusbar.show = 'in-mode'
 
-# When to show the tab bar.
-# Type: String
-# Valid values:
-#   - always: Always show the tab bar.
-#   - never: Always hide the tab bar.
-#   - multiple: Hide the tab bar if only one tab is open.
-#   - switching: Show the tab bar when switching tabs.
 c.tabs.show = 'multiple'
