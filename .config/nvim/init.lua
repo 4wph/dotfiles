3c84e145440caae2ec6952ddf53ca41eb269c680
a2lqr4 <gustavoaraujob1@gmail.com>
2020-12-05 11:27:48 -0300

    nvim update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,5 @@
 require('packer').startup(function()
+	use 'wbthomason/packer.nvim'
 	use 'jiangmiao/auto-pairs'
 	use 'justinmk/vim-sneak'
 	use 'guns/vim-sexp'
@@ -13,25 +14,27 @@ require('packer').startup(function()
 
 	use 'neovim/nvim-lspconfig'
 	use 'nvim-lua/completion-nvim'
-
-	use 'hrsh7th/vim-vsnip'
-	use 'hrsh7th/vim-vsnip-integ'
-	use 'SirVer/ultisnips'
-	use 'honza/vim-snippets'
 end)
 
 vim.cmd('colorscheme snazzy')-- FIXME
 vim.cmd('autocmd BufEnter * lua require\'completion\'.on_attach()')-- FIXME
-vim.cmd('command! Format execute \'lua vim.lsp.buf.formatting()\'')
 
-vim.g.UltiSnipsJumpForwardTrigger = "<C-f>"
-vim.g.UltiSnipsJumpBackwardTrigger = "<C-b>"
 vim.g.ctrlp_user_command = { '.git', 'cd %s && git ls-files -co --exclude-standard' }
-vim.g.completion_enable_snippet = 'UltiSnips'
 vim.g.completion_matching_strategy_list = { 'exact', 'substring', 'fuzzy' }
 
 vim.o.cot = "menuone,noinsert,noselect"
 vim.o.termguicolors = true
+
+vim.cmd('command! Format execute \'lua vim.lsp.buf.formatting()\'')-- FIXME
+vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<Leader>xr', '<cmd>lua vim.lsp.buf.rename()<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<Leader>xd', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', {noremap = true})
+vim.api.nvim_set_keymap('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {noremap = true, expr = true})
+vim.api.nvim_set_keymap('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<S-Tab>"', {noremap = true, expr = true})
 
 local lspconfig = require'lspconfig'
 local configs = require('lspconfig/configs')
@@ -60,11 +63,3 @@ require'nvim-treesitter.configs'.setup {
     enable = true
   }
 }
-
-vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', {})
-vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', {})
-vim.api.nvim_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', {})
-vim.api.nvim_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', {})
-vim.api.nvim_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', {})
-vim.api.nvim_set_keymap('n', '<Leader>xr', '<cmd>lua vim.lsp.buf.rename()<CR>', {})
-vim.api.nvim_set_keymap('n', '<Leader>xd', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', {})
