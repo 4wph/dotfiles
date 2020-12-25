d3edc8d43f8329cd45a9139a63d79d37a94ec07c
4wph <gustavoaraujob1@gmail.com>
2020-12-24 23:45:02 -0300

    Segregate emacs config

--- .config/emacs/init.el
+++
@@ -1,9 +0,0 @@
-(menu-bar-mode -1)
-
-(setq
- make-backup-files nil
- create-lockfiles nil
- auto-save-default nil
- custom-file "~/.config/emacs/custom.el")
-
-(global-set-key (kbd "C-c i") (lambda () (interactive) (find-file user-init-file)))
